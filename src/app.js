bbc627406dafad81aa6be5b28fa2b371cc2999fc
Roobsk91 <rubyasmith91@gmail.com>
2023-07-13 19:14:22 +0100

    added temp unit conversion

--- src/app.js
+++ src/app.js
@@ -14,8 +14,9 @@ let day = days[date.getDay()];
 }
 
 function displayTemperature(response) {
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+celsiusTemperature = response.data.temperature.current;
+    let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.city;
 let descriptionElement = document.querySelector("#description");
@@ -43,9 +44,32 @@ function handleSubmit(event) {
     search(cityInputElement.value);
     }
 
-    search("London");
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("London");
